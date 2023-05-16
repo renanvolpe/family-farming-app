1ada308383c748781c3767d42d6de3c5bad274f2
gaschrader <gaschrader02@gmail.com>
2023-05-16 17:13:13 -0300

    fix scroll issues in login screen

--- lib/modules/authentication/pages/login_page.dart
+++ lib/modules/authentication/pages/login_page.dart
@@ -17,214 +17,233 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      resizeToAvoidBottomInset: false,
       appBar: AppBar(
         primary: false,
         elevation: 0,
         backgroundColor: const Color.fromRGBO(0, 0, 0, 0),
         centerTitle: true,
-        toolbarHeight: 240,
-        title: Padding(
-          padding: const EdgeInsets.only(top: 70.0),
-          child: Image.asset(
-            'lib/core/images/placeholder.png',
-            fit: BoxFit.contain,
-            height: 150,
-          ),
-        ),
-      ),
-      body: Column(
-        children: [
-          const Text(
-            'Entre em sua conta',
-            textAlign: TextAlign.left,
-            style: TextStyle(
-              color: Color.fromRGBO(41, 45, 50, 1.0),
-              fontSize: 40,
-              fontWeight: FontWeight.w500,
-              fontFamily: 'Abhaya Libre',
-            ),
-          ),
-          Container(
-            padding: const EdgeInsets.only(top: 40, right: 180),
-            child: const SizedBox(
-              width: 170.0,
-              height: 25.0,
-              child: Text(
-                'Email',
-                textAlign: TextAlign.left,
-                style: TextStyle(
-                  color: Color.fromRGBO(41, 45, 50, 1.0),
-                  fontSize: 17,
-                  fontWeight: FontWeight.w500,
-                  fontFamily: 'Abhaya Libre',
-                ),
+        toolbarHeight: 320,
+        title: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.only(top: 110.0, bottom: 25.0),
+              child: Image.asset(
+                'lib/core/images/placeholder.png',
+                fit: BoxFit.contain,
+                height: 150,
               ),
             ),
-          ),
-          SizedBox(
-            width: 350.0,
-            child: TextFormField(
-              decoration: const InputDecoration(
-                border: UnderlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Color.fromRGBO(179, 191, 203, 1.0),
-                  ),
-                ),
-                hintText: "Insira seu email",
-                hintStyle: TextStyle(
-                  color: Color.fromRGBO(179, 191, 203, 1.0),
-                  fontSize: 18,
-                  fontWeight: FontWeight.w500,
-                  fontFamily: 'Abhaya Libre',
-                ),
-              ),
-            ),
-          ),
-          Container(
-            padding: const EdgeInsets.only(top: 40, right: 180),
-            child: const SizedBox(
-              width: 170.0,
-              height: 25.0,
-              child: Text(
-                'Senha',
-                textAlign: TextAlign.left,
-                style: TextStyle(
-                  color: Color.fromRGBO(41, 45, 50, 1.0),
-                  fontSize: 18,
-                  fontWeight: FontWeight.w500,
-                  fontFamily: 'Abhaya Libre',
-                ),
-              ),
-            ),
-          ),
-          SizedBox(
-            width: 350.0,
-            child: TextFormField(
-              decoration: const InputDecoration(
-                border: UnderlineInputBorder(
-                  borderSide: BorderSide(
-                    color: Color.fromRGBO(179, 191, 203, 1.0),
-                  ),
-                ),
-                hintText: "Insira sua senha",
-                hintStyle: TextStyle(
-                  color: Color.fromRGBO(179, 191, 203, 1.0),
-                  fontSize: 18,
-                  fontWeight: FontWeight.w500,
-                  fontFamily: 'Abhaya Libre',
-                ),
-              ),
-            ),
-          ),
-          const SizedBox(
-            width: 350,
-            height: 50,
-            child: Text(
-              'Esqueci minha senha',
-              textAlign: TextAlign.end,
+            const Text(
+              'Entre em sua conta',
+              textAlign: TextAlign.left,
               style: TextStyle(
-                color: Color.fromRGBO(106, 121, 138, 1.0),
-                fontSize: 17,
-                height: 2.9,
+                color: Color.fromRGBO(41, 45, 50, 1.0),
+                fontSize: 40,
                 fontWeight: FontWeight.w500,
                 fontFamily: 'Abhaya Libre',
-                decoration: TextDecoration.underline,
               ),
             ),
-          ),
-          Container(
-            margin: const EdgeInsets.all(30),
-            width: 348,
-            height: 58,
-            child: FilledButton(
-              onPressed: () {
-                //button´s function
-                Navigator.push(
-                  context,
-                  MaterialPageRoute<void>(
-                    builder: (BuildContext context) => const HomeMain(),
+          ],
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+              padding: const EdgeInsets.only(top: 20, right: 180),
+              child: const SizedBox(
+                width: 170.0,
+                height: 25.0,
+                child: Text(
+                  'Email',
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                    color: Color.fromRGBO(41, 45, 50, 1.0),
+                    fontSize: 17,
+                    fontWeight: FontWeight.w500,
+                    fontFamily: 'Abhaya Libre',
                   ),
-                );
-              },
-              style: ButtonStyle(
-                backgroundColor: const MaterialStatePropertyAll<Color>(
-                  Color.fromRGBO(0, 48, 70, 1.0),
-                ),
-                shape: MaterialStatePropertyAll<RoundedRectangleBorder>(
-                  RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15),
-                  ),
-                ),
-              ),
-              child: const Text(
-                "Faça login",
-                style: TextStyle(
-                  color: Color.fromRGBO(255, 255, 255, 1.0),
-                  fontWeight: FontWeight.w500,
-                  fontFamily: 'Abhaya Libre',
-                  fontSize: 18,
                 ),
               ),
             ),
-          ),
-          const Row(
-            children: <Widget>[
-              Expanded(
-                child: Divider(
-                  color: Color.fromARGB(255, 196, 194, 194),
-                ),
-              ),
-              Text(
-                "Ou",
-                style: TextStyle(
-                  fontFamily: 'Abhaya Libre',
-                  fontWeight: FontWeight.w500,
-                  fontSize: 18,
-                ),
-              ),
-              Expanded(
-                child: Divider(
-                  color: Color.fromARGB(255, 196, 194, 194),
-                ),
-              ),
-            ],
-          ),
-          Container(
-            margin: const EdgeInsets.all(30),
-            width: 348,
-            height: 58,
-            child: FilledButton(
-              onPressed: () {
-                //button´s function
-                Navigator.push(
-                  context,
-                  MaterialPageRoute<void>(
-                    builder: (BuildContext context) => const RegisterPage(),
+            SizedBox(
+              width: 350.0,
+              child: TextFormField(
+                decoration: const InputDecoration(
+                  border: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Color.fromRGBO(179, 191, 203, 1.0),
+                    ),
                   ),
-                );
-              },
-              style: ButtonStyle(
-                backgroundColor: const MaterialStatePropertyAll<Color>(
-                  Color.fromRGBO(133, 202, 228, 1.0),
-                ),
-                shape: MaterialStatePropertyAll<RoundedRectangleBorder>(
-                  RoundedRectangleBorder(
-                    borderRadius: BorderRadius.circular(15),
+                  hintText: "Insira seu email",
+                  hintStyle: TextStyle(
+                    color: Color.fromRGBO(179, 191, 203, 1.0),
+                    fontSize: 18,
+                    fontWeight: FontWeight.w500,
+                    fontFamily: 'Abhaya Libre',
                   ),
-                ),
-              ),
-              child: const Text(
-                "Cadastre-se",
-                style: TextStyle(
-                  color: Color.fromRGBO(21, 23, 25, 1.0),
-                  fontWeight: FontWeight.w500,
-                  fontFamily: 'Abhaya Libre',
-                  fontSize: 18,
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Color.fromRGBO(41, 45, 50, 1.0),
+                    ),
+                  ),
                 ),
               ),
             ),
-          ),
-        ],
+            Container(
+              padding: const EdgeInsets.only(top: 40, right: 180),
+              child: const SizedBox(
+                width: 170.0,
+                height: 25.0,
+                child: Text(
+                  'Senha',
+                  textAlign: TextAlign.left,
+                  style: TextStyle(
+                    color: Color.fromRGBO(41, 45, 50, 1.0),
+                    fontSize: 18,
+                    fontWeight: FontWeight.w500,
+                    fontFamily: 'Abhaya Libre',
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(
+              width: 350.0,
+              child: TextFormField(
+                decoration: const InputDecoration(
+                  border: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Color.fromRGBO(179, 191, 203, 1.0),
+                    ),
+                  ),
+                  hintText: "Insira sua senha",
+                  hintStyle: TextStyle(
+                    color: Color.fromRGBO(179, 191, 203, 1.0),
+                    fontSize: 18,
+                    fontWeight: FontWeight.w500,
+                    fontFamily: 'Abhaya Libre',
+                  ),
+                  focusedBorder: UnderlineInputBorder(
+                    borderSide: BorderSide(
+                      color: Color.fromRGBO(41, 45, 50, 1.0),
+                    ),
+                  ),
+                ),
+              ),
+            ),
+            const SizedBox(
+              width: 350,
+              height: 50,
+              child: Text(
+                'Esqueci minha senha',
+                textAlign: TextAlign.end,
+                style: TextStyle(
+                  color: Color.fromRGBO(106, 121, 138, 1.0),
+                  fontSize: 17,
+                  height: 2.9,
+                  fontWeight: FontWeight.w500,
+                  fontFamily: 'Abhaya Libre',
+                  decoration: TextDecoration.underline,
+                ),
+              ),
+            ),
+            Container(
+              margin: const EdgeInsets.all(30),
+              width: 348,
+              height: 58,
+              child: FilledButton(
+                onPressed: () {
+                  //button´s function
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute<void>(
+                      builder: (BuildContext context) => const HomeMain(),
+                    ),
+                  );
+                },
+                style: ButtonStyle(
+                  backgroundColor: const MaterialStatePropertyAll<Color>(
+                    Color.fromRGBO(0, 48, 70, 1.0),
+                  ),
+                  shape: MaterialStatePropertyAll<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                  ),
+                ),
+                child: const Text(
+                  "Faça login",
+                  style: TextStyle(
+                    color: Color.fromRGBO(255, 255, 255, 1.0),
+                    fontWeight: FontWeight.w500,
+                    fontFamily: 'Abhaya Libre',
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+            ),
+            const Row(
+              children: <Widget>[
+                Expanded(
+                  child: Divider(
+                    color: Color.fromARGB(213, 222, 231, 194),
+                    indent: 30,
+                  ),
+                ),
+                Text(
+                  "Ou",
+                  style: TextStyle(
+                    fontFamily: 'Abhaya Libre',
+                    fontWeight: FontWeight.w500,
+                    fontSize: 18,
+                  ),
+                ),
+                Expanded(
+                  child: Divider(
+                    color: Color.fromARGB(213, 222, 231, 194),
+                    endIndent: 30,
+                  ),
+                ),
+              ],
+            ),
+            Container(
+              margin: const EdgeInsets.all(30),
+              width: 348,
+              height: 58,
+              child: FilledButton(
+                onPressed: () {
+                  //button´s function
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute<void>(
+                      builder: (BuildContext context) => const RegisterPage(),
+                    ),
+                  );
+                },
+                style: ButtonStyle(
+                  backgroundColor: const MaterialStatePropertyAll<Color>(
+                    Color.fromRGBO(133, 202, 228, 1.0),
+                  ),
+                  shape: MaterialStatePropertyAll<RoundedRectangleBorder>(
+                    RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(15),
+                    ),
+                  ),
+                ),
+                child: const Text(
+                  "Cadastre-se",
+                  style: TextStyle(
+                    color: Color.fromRGBO(21, 23, 25, 1.0),
+                    fontWeight: FontWeight.w500,
+                    fontFamily: 'Abhaya Libre',
+                    fontSize: 18,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
