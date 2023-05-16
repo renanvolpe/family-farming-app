b2b17d2942809315999343c03b80711625db1080
gaschrader <gaschrader02@gmail.com>
2023-05-16 12:03:53 -0300

    add some new changes to login screen

--- lib/modules/authentication/pages/login_page.dart
+++ lib/modules/authentication/pages/login_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:organaki_app/modules/authentication/pages/register_page.dart';
+import 'package:organaki_app/modules/home/pages/home_main.dart';
 
 class LoginPage extends StatefulWidget {
   const LoginPage({Key? key}) : super(key: key);
@@ -17,8 +18,8 @@ class _LoginPageState extends State<LoginPage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Tela login"),
-        centerTitle: true,
+        elevation: 0,
+        backgroundColor: const Color.fromRGBO(0, 0, 0, 0),
       ),
       body: Column(
         children: [
@@ -29,6 +30,7 @@ class _LoginPageState extends State<LoginPage> {
               color: Color.fromRGBO(41, 45, 50, 1.0),
               fontSize: 40,
               fontWeight: FontWeight.w500,
+              fontFamily: 'Abhaya Libre',
             ),
           ),
           Container(
@@ -43,6 +45,7 @@ class _LoginPageState extends State<LoginPage> {
                   color: Color.fromRGBO(41, 45, 50, 1.0),
                   fontSize: 17,
                   fontWeight: FontWeight.w500,
+                  fontFamily: 'Abhaya Libre',
                 ),
               ),
             ),
@@ -55,6 +58,7 @@ class _LoginPageState extends State<LoginPage> {
                 color: Color.fromRGBO(179, 191, 203, 1.0),
                 fontSize: 17,
                 fontWeight: FontWeight.w500,
+                fontFamily: 'Abhaya Libre',
               ),
             ),
           ),
@@ -70,6 +74,7 @@ class _LoginPageState extends State<LoginPage> {
                   color: Color.fromRGBO(41, 45, 50, 1.0),
                   fontSize: 17,
                   fontWeight: FontWeight.w500,
+                  fontFamily: 'Abhaya Libre',
                 ),
               ),
             ),
@@ -81,28 +86,77 @@ class _LoginPageState extends State<LoginPage> {
                 color: Color.fromRGBO(179, 191, 203, 1.0),
                 fontSize: 17,
                 fontWeight: FontWeight.w500,
+                fontFamily: 'Abhaya Libre',
               ),
             ),
           ),
           Container(
-            color: const Color.fromRGBO(133, 202, 228, 1.0),
             margin: const EdgeInsets.all(30),
-            child: TextButton(
-                onPressed: () {
-                  //button´s function
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute<void>(
-                      builder: (BuildContext context) => const RegisterPage(),
-                    ),
-                  );
-                },
-                child: const Text(
-                  "Ainda não é usuário? Cadastre-se!",
-                  style: TextStyle(
-                    color: Color.fromRGBO(21, 23, 25, 1.0),
+            width: 348,
+            height: 58,
+            child: FilledButton(
+              onPressed: () {
+                //button´s function
+                Navigator.push(
+                  context,
+                  MaterialPageRoute<void>(
+                    builder: (BuildContext context) => const HomeMain(),
                   ),
-                )),
+                );
+              },
+              style: ButtonStyle(
+                backgroundColor: const MaterialStatePropertyAll<Color>(
+                  Color.fromRGBO(0, 48, 70, 1.0),
+                ),
+                shape: MaterialStatePropertyAll<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                ),
+              ),
+              child: const Text(
+                "Fazer login",
+                style: TextStyle(
+                  color: Color.fromRGBO(255, 255, 255, 1.0),
+                  fontWeight: FontWeight.w500,
+                  fontFamily: 'Abhaya Libre',
+                ),
+              ),
+            ),
+          ),
+          Container(
+            margin: const EdgeInsets.all(30),
+            width: 348,
+            height: 58,
+            child: FilledButton(
+              onPressed: () {
+                //button´s function
+                Navigator.push(
+                  context,
+                  MaterialPageRoute<void>(
+                    builder: (BuildContext context) => const RegisterPage(),
+                  ),
+                );
+              },
+              style: ButtonStyle(
+                backgroundColor: const MaterialStatePropertyAll<Color>(
+                  Color.fromRGBO(133, 202, 228, 1.0),
+                ),
+                shape: MaterialStatePropertyAll<RoundedRectangleBorder>(
+                  RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(15),
+                  ),
+                ),
+              ),
+              child: const Text(
+                "Ainda não é usuário? Cadastre-se!",
+                style: TextStyle(
+                  color: Color.fromRGBO(21, 23, 25, 1.0),
+                  fontWeight: FontWeight.w500,
+                  fontFamily: 'Abhaya Libre',
+                ),
+              ),
+            ),
           ),
         ],
       ),
