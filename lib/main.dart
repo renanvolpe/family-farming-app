d35b0430925a6b40b3f06c2e0ccd3950bc499d15
Volpe <renan.volpe@aluno.ifsp.edu.br>
2023-06-01 18:58:20 -0300

    make call mock api of list producers in home

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:organaki_app/modules/authentication/bloc/login_auth_bloc/login_auth_bloc.dart';
+import 'package:organaki_app/modules/home/bloc/bloc_get_list_producer/get_list_producers_bloc.dart';
 import 'package:organaki_app/modules/home/pages/home_main.dart';
 import 'package:organaki_app/services/authentication_services.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:organaki_app/services/producer_services.dart';
 
 void main() {
-  runApp(BlocProvider(
-    create: (context) => LoginAuthBloc(AuthenticationRepository()),
+  runApp(MultiBlocProvider(
+    providers: [
+      BlocProvider(
+        create: (context) => LoginAuthBloc(AuthenticationRepository()),
+      ),
+      BlocProvider(
+        create: (context) => GetListProducersBloc(ProducerRepository()),
+      ),
+    ],
     child: MaterialApp(
       home: const HomeMain(),
       theme: ThemeData(useMaterial3: true),
---
+++ lib/services/producer_services.dart
@@ -0,0 +1,72 @@
+import 'package:dio/dio.dart';
+import 'package:organaki_app/core/endpoints.dart';
+import 'package:organaki_app/models/producer.dart';
+import 'package:organaki_app/models/user.dart';
+import 'package:result_dart/result_dart.dart';
+
+abstract class ProducerService {
+  Future<Result<List<Producer>, String>> getListProducers();
+  getAProducer();
+  editProducer();
+  deleteProducer();
+}
+
+class ProducerRepository implements ProducerService {
+  final dio = Dio();
+  String? errorMessage;
+
+  @override
+  Future<Result<List<Producer>, String>> getListProducers() async {
+    //Map<String, dynamic>? params = {"username":username, "password": password};
+    // Map<String, dynamic>? header = {};
+    // Map body = {};
+    var response = await dio.get(
+      Endpoints.baseUrlMock + Endpoints.listProducersMock,
+      // queryParameters: params, data: body, options: Options(headers: header)
+    );
+    try {
+      if (response.statusCode == 200) {
+        List<Producer> listProducer = [];
+        for (var element in response.data) {
+          listProducer.add(Producer.fromMap(element));
+        }
+        return Success(listProducer);
+      } else if (response.statusCode == 400) {
+        errorMessage = "Chamada feita de maneira errada";
+      } else if (response.statusCode == 500) {
+        errorMessage = "Sistema fora do ar, contate o adiminstrador";
+      }
+    } catch (e) {
+      print(
+          "${response.statusCode} Error ProducerRepository ::  getListProducers :: $e ");
+      errorMessage = "Erro do sistema";
+    }
+    return Failure(errorMessage ?? "Erro não esperado");
+  }
+
+  registerUser() {
+    throw UnimplementedError();
+  }
+
+  setupServiceUser() {
+    throw UnimplementedError();
+  }
+
+  @override
+  deleteProducer() {
+    // TODO: implement deleteProducer
+    throw UnimplementedError();
+  }
+
+  @override
+  editProducer() {
+    // TODO: implement editProducer
+    throw UnimplementedError();
+  }
+
+  @override
+  getAProducer() {
+    // TODO: implement getAProducer
+    throw UnimplementedError();
+  }
+}
